a51ef2fafaa6e490a710280360e6bcfd6369d1af
SUJON AHMED <ensujon01@gmail.com>
2025-11-20 05:04:43 +0600

    Implement Basic For Loop

---
+++ For_Loop_Basic.py
@@ -0,0 +1,25 @@
+print('Basic For Loop')
+container = [5,10,15,20,25,30]
+
+for num in container :
+    print(num)
+
+# Summation Using Loop
+
+val = [1,2,3,4,5,6,7,8,9]
+sum = 0
+for digit in val :
+    sum = sum + digit
+
+print(sum)
+
+# Range (start,stop) 
+# start -> Inclusive , stop -> exclusive
+
+for access in range(1,10) :
+    print(access)
+
+
+for incriment in range(1,10,2) : #(start,stop,step)
+    print(incriment)
+
