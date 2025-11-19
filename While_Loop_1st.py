e7936868dcd6de1fd2df2f99f112f08b20f128c3
SUJON AHMED <ensujon01@gmail.com>
2025-11-20 05:01:46 +0600

    Implement while loop examples with break and continue
    Added examples of while loops, including break and continue statements.

---
+++ While_Loop_1st.py
@@ -0,0 +1,24 @@
+# Here normal loop
+num = 1
+while num <=10:
+    print(num)
+    num = num+1
+
+
+# Break and Continue
+
+me = 1  
+while me < 10 :
+    print(me)
+    me = me + 1
+
+    if me == 7 :
+        break
+
+
+conti = 0
+while conti <=10 :
+    conti = conti + 1
+    if conti % 2 == 1 :
+        continue
+    print(conti)
